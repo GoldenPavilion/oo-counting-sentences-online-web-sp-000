6b3f3b93ba4ee817fd974db182c9b346a059a2e2
Robert Stine <rcorystine@gmail.com>
2021-01-04 22:35:18 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,6 +27,7 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).delete_if{|word| word.size > 2}.size 
+    self.split(/\.|\?|\!/).count 
+    #delete_if{|word| word.size > 2}.size 
   end
 end
